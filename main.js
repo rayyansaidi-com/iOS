86e4f7889e6c60fda2fddd8b595c3b50124d0ecd
Nathan Chu <63111210+nthnchu@users.noreply.github.com>
2020-08-18 19:33:42 -0400

    Add apple fonts, make sure caching doesn't work on dev

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .then(res => console.log("INFO:", res))
-      .catch(err => console.error("ERROR:", err))
-  })
+const isDev = true;
+const notDev = !isDev;
+
+if (notDev) {
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+      navigator.serviceWorker
+        .register("/sw.js")
+        .then(res => console.log("INFO:", res))
+        .catch(err => console.error("ERROR:", err))
+    })
+  }
 }
