d1ca6353964d8cb72936287ac03bc03050ca2b92
Nathan Chu <63111210+nthnchu@users.noreply.github.com>
2020-08-18 19:03:57 -0400

    Finish PWA caching

---
+++ main.js
@@ -0,0 +1,8 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(res => console.log("INFO:", res))
+      .catch(err => console.error("ERROR:", err))
+  })
+}
